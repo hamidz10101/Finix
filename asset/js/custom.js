bad0431af87de7a76fc9664c337070edb6f23914
Hamid Rahman <61022400+hamidz10101@users.noreply.github.com>
2020-09-14 21:11:29 +0600

    Add files via upload

---
+++ asset/js/custom.js
@@ -0,0 +1,109 @@
+$('.owl-carousel').owlCarousel({
+    margin:80,
+    loop: true,
+    nav: false,
+    dots: false,
+    smartSpeed: 2000,
+    autoplayHoverPause: true,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        576: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1024: {
+            items: 3
+        },
+        1200: {
+            items: 4
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+	
+
+		// Projects Slider
+		// $('.client-word ').owlCarousel({
+		// 	loop: true,
+		// 	nav: true,
+		// 	dots: false,
+		// 	smartSpeed: 500,
+		// 	margin: 30,
+		// 	autoplayHoverPause: true,
+		// 	autoplay: true,
+		// 	navText: [
+		// 		"<i class='flaticon-left-arrow'></i>",
+		// 		"<i class='flaticon-right-arrow'></i>"
+		// 	],
+		// 	responsive: {
+		// 		0: {
+		// 			items: 1
+		// 		},
+		// 		576: {
+		// 			items: 1
+		// 		},
+		// 		768: {
+		// 			items: 2
+		// 		},
+		// 		1024: {
+		// 			items: 3
+		// 		},
+		// 		1200: {
+		// 			items: 4
+		// 		}
+		// 	}
+		// });
+
+
+
+
+
+
+
+
+
+
+
+		// Partner Slider
+		// $('.partner-slider').owlCarousel({
+		// 	loop: true,
+		// 	nav: false,
+		// 	dots: false,
+		// 	smartSpeed: 2000,
+		// 	margin: 30,
+		// 	autoplayHoverPause: true,
+        //     autoplay: true,
+		// 	responsive: {
+		// 		0: {
+		// 			items: 1
+		// 		},
+		// 		576: {
+		// 			items: 1
+		// 		},
+		// 		768: {
+		// 			items: 3
+		// 		},
+		// 		1024: {
+		// 			items: 4
+		// 		},
+		// 		1200: {
+		// 			items: 5
+		// 		}
+		// 	}
+		// });
+
+	
